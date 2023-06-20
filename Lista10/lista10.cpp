6e10273cfe1dae2dd04d76a8f50b863cb2ab80c1
ayk <arturxavierns@gmail.com>
2023-06-19 21:21:37 -0300

    Arrumei o return da lista 10

--- Lista10/lista10.cpp
+++ Lista10/lista10.cpp
@@ -151,7 +151,7 @@ bool Circunferencia::contem(Triangulo& t)
         {
             return true;
         }
-        else false;
+        else return false;
 }
 
 bool Circunferencia::contem(Retangulo& r)
@@ -260,4 +260,3 @@ int main(){
 
     return (0);
 }
-
